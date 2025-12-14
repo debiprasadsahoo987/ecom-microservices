a33ab1987e3e4db31865c69a91e464bbd4651567
Debiprasad <debiprasadsahoo987@gmail.com>
2025-12-14 23:24:34 +0530

    few changes

--- user/src/main/java/com/ecom/app/user/controllers/UserController.java
+++ user/src/main/java/com/ecom/app/user/controllers/UserController.java
@@ -3,6 +3,7 @@ package com.ecom.app.user.controllers;
 import com.ecom.app.user.payload.UserRequestDTO;
 import com.ecom.app.user.payload.UserResponseDTO;
 import com.ecom.app.user.services.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -30,6 +32,12 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponseDTO> getUserById(@PathVariable String id) {
+        log.info("Request received for user id {}", id);
+        log.info("Info Level Logs");
+        log.trace("Trace Level Logs");
+        log.debug( "Debug Level Logs");
+        log.warn( "Warn Level Logs");
+        log.error( "Error Level Logs");
         return  userService.getUserById(id).map(ResponseEntity::ok).orElseGet(()->ResponseEntity.notFound().build());
     }
 
